cb35884f2b19fff931c08f4a921815d816598ea0
Daniel Patterson <dan24patt@gmail.com>
2016-07-01 23:45:36 +0100

    Use WeatherTypes as test inputs
    These should show different icons for different WeatherTypes eventually.

--- graphical-test/graphical-test.js
+++ graphical-test/graphical-test.js
@@ -1,3 +1,45 @@
+import WeatherType from 'weather-type';
+
 import {draw} from '../dist/weather-icons';
 
-draw('#icon');
+const codes = [
+  'thunderstorm',
+  'heavy_hail',
+  'heavy_snow',
+  'heavy_sleet',
+  'heavy_rain',
+  'heavy_hail_showers',
+  'heavy_snow_showers',
+  'heavy_sleet_showers',
+  'heavy_rain_showers',
+  'light_hail',
+  'light_snow',
+  'light_sleet',
+  'light_rain',
+  'light_hail_showers',
+  'light_snow_showers',
+  'light_sleet_showers',
+  'light_rain_showers',
+  'Fog',
+  'Mist',
+  'heavy_clouds',
+  'light_clouds',
+  'broken_clouds',
+  'clear'
+]
+
+const list = document.createElement('UL');
+document.body.appendChild(list);
+
+codes.forEach(code => {
+  const listItem = document.createElement('LI');
+  list.appendChild(listItem);
+  const iconDiv = document.createElement('DIV');
+  const textDiv = document.createElement('DIV');
+  iconDiv.id = code;
+  listItem.appendChild(iconDiv);
+  listItem.appendChild(textDiv);
+  textDiv.textContent = code;
+  console.log(code, WeatherType.fromString(code).value);
+  draw(`#${code}`, WeatherType.fromString(code).value);
+});
