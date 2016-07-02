6b146369adc9c60ede7a738aa524705d8e1ae479
Daniel Patterson <dan24patt@gmail.com>
2016-07-02 15:16:22 +0100

    Improve display of graphical test
    So that icons are displayed in an order that shows the progression
    through different conditions.

--- graphical-test/graphical-test.js
+++ graphical-test/graphical-test.js
@@ -3,42 +3,48 @@ import WeatherType from 'weather-type';
 import {draw} from '../dist/weather-icons';
 
 const codes = [
-  'thunderstorm',
-  'heavy_hail',
-  'heavy_snow',
-  'heavy_sleet',
-  'heavy_rain',
-  'heavy_hail_showers',
-  'heavy_snow_showers',
-  'heavy_sleet_showers',
-  'heavy_rain_showers',
-  'light_hail',
-  'light_snow',
-  'light_sleet',
-  'light_rain',
-  'light_hail_showers',
-  'light_snow_showers',
-  'light_sleet_showers',
-  'light_rain_showers',
-  'Fog',
+  'Clear',
+  'Light rain showers',
+  'Light rain',
+  'Heavy rain showers',
+  'Heavy rain',
   'Mist',
-  'heavy_clouds',
-  'light_clouds',
-  'broken_clouds',
-  'clear'
+  'Broken clouds',
+  'Light sleet showers',
+  'Light sleet',
+  'Heavy sleet showers',
+  'Heavy sleet',
+  'Fog',
+  'Light clouds',
+  'Light snow showers',
+  'Light snow',
+  'Heavy snow showers',
+  'Heavy snow',
+  'Thunderstorm',
+  'Heavy clouds',
+  'Light hail showers',
+  'Light hail',
+  'Heavy hail showers',
+  'Heavy hail'
 ]
 
 const list = document.createElement('UL');
 document.body.appendChild(list);
 
-codes.forEach(code => {
-  const listItem = document.createElement('LI');
-  list.appendChild(listItem);
-  const iconDiv = document.createElement('DIV');
-  const textDiv = document.createElement('DIV');
-  iconDiv.id = code;
-  listItem.appendChild(iconDiv);
-  listItem.appendChild(textDiv);
-  textDiv.textContent = code;
-  draw(WeatherType.fromString(code).value, `#${code}`);
+codes.forEach(text => {
+  WeatherType.fromString(text).map(weatherType => {
+
+    const listItem = document.createElement('LI');
+    list.appendChild(listItem);
+
+    const iconDiv = document.createElement('DIV');
+    const textElement = document.createElement('P');
+    listItem.appendChild(iconDiv);
+    listItem.appendChild(textElement);
+
+    iconDiv.id = weatherType.toCode();
+    textElement.textContent = text;
+
+    draw(weatherType, `#${iconDiv.id}`);
+  });
 });
